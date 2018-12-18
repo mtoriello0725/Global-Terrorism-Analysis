64cc8ae7c7c4041689c42ed67c5ec9289f4faf11
mtoriello0725 <34101012+mtoriello0725@users.noreply.github.com>
2018-12-18 16:06:16 -0500

    removed old code, and moved menu bar for leaflet map to top left side

--- static/js/app.js
+++ static/js/app.js
@@ -1,47 +1,3 @@
-/*
-var myMap = L.map("geoMap", {
-  center: [31, 65],
-  zoom: 2
-});
-
-
-// Adding tile layer to the map
-var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 8,
-  id: "mapbox.streets",
-  accessToken: API_KEY
-}).addTo(myMap);
-
-function globalTerrorismMap() {
-
-	URL = "/api/geography";
-	d3.json(URL, function(response) {
-
-		var markers = L.markerClusterGroup();
-
-		var geoJsonLayer = L.geoJson(response, {
-			onEachFeature: function(feature, layer) {
-				layer.bindPopup(feature.properties.target_type);
-				layer.on("mouseover", function(e) {
-					this.openPopup();
-				});
-				layer.on("mouseout", function(e) {
-					this.closePopup();
-				});
-			}
-		});
-
-		markers.addLayer(geoJsonLayer);
-
-		myMap.addLayer(markers);
-		myMap.fitBounds(markers.getBounds());
-	});
-}
-
-globalTerrorismMap()
-*/
-
 function getYearMap(year) {
 
 	URL = "/api/geography/"+year;
@@ -54,7 +10,7 @@ function getYearMap(year) {
 
 		var geoJsonLayer = L.geoJson(yearData, {
 			onEachFeature: function(feature, layer) {
-				layer.bindPopup("<strong>"+feature.properties.target_type+"</strong><br><b>"+feature.properties.attack_type+"</b>");
+				layer.bindPopup("<strong>Target Type: </strong><p>"+feature.properties.target_type+"</p><br><strong>Attack Type: </strong><p>"+feature.properties.attack_type+"</p>");
 				layer.on("mouseover", function(e) {
 					this.openPopup();
 				});
@@ -75,6 +31,7 @@ function getYearMap(year) {
 		var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
 		attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
 		maxZoom: 8,
+		minZoom: 2,
 		id: "mapbox.streets",
 		accessToken: API_KEY
 		});
@@ -82,6 +39,7 @@ function getYearMap(year) {
 		var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
 		attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
 		maxZoom: 8,
+		minZoom: 2,
 		id: "mapbox.dark",
 		accessToken: API_KEY
 		});
@@ -107,9 +65,7 @@ function getYearMap(year) {
 		// Create a layer control
 		// Pass in our baseMaps and overlayMaps
 		// Add the layer control to the map
-		L.control.layers(baseMaps, overlayMaps, {
-		collapsed: false
-		}).addTo(myMap);
+		L.control.layers(baseMaps, overlayMaps, {position: "topleft"}).addTo(myMap);
 	}
 }
 
