44b03074456c1e2532b2343e9b102e18bf730341
mtoriello0725 <34101012+mtoriello0725@users.noreply.github.com>
2018-12-19 13:13:40 -0500

    delete config file

---
+++ static/js/config.js
@@ -0,0 +1,3 @@
+// API key
+const API_KEY = "pk.eyJ1IjoibXRvcmllbGxvMDcyNSIsImEiOiJjanA1bHphZjQwOXFwM3dxczlwcDI1azVpIn0.Bv4CbKtF6bV6JbSMfjCIgQ";
+// const API_KEY = "pk.eyJ1IjoibXRvcmllbGxvMDcyNSIsImEiOiJjanA1bTEzZTMwdGduM3BwN2p0MXNpemZtIn0.Zc9q8gCG-uo6scqGqyD9fQ";
